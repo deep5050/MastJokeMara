b93d89df074c4f369ef49d2b015799f305dff335
Dipankar Pal <dipankarpal5050@gmail.com>
2020-10-13 21:44:53 +0530

    Support for both Issues and PRs (#28)
    * PR support
    
    * fix: comment body message
    
    * readme updated
    
    * Fix: PR message body

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Axios = require('axios');
 console.log("workflow started....");
 
 async function getRandomJoke() {
-    const url = "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Dark,Pun?blacklistFlags=nsfw,religious,political,racist,sexist"
+    const url = "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist"
     try {
         const response = await Axios.get(url);
         return parseJoke(response.data)
@@ -24,56 +24,59 @@ function parseJoke(jsonData) {
         jokeMarkDown = data.joke
     }
     else if (data.type === 'twopart') {
-        jokeMarkDown = data.setup + "\n\n" + data.delivery;
+        jokeMarkDown = data.setup + "\n" + data.delivery;
     }
     return jokeMarkDown
-
 }
 
 
 async function run(joke) {
     try {
         const github_token = core.getInput('GITHUB_TOKEN');
-
         const context = github.context;
+        var event = github.context.eventName;
+        var greetMsg;
+        if (event === 'pull_request') {
+            greetMsg = 'Thanks for opening this PR :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will review it :eyes: and get back to you as soon as possible :+1: . Just make sure you have followed the contribution guidelines.\n\nBy that time enjoy this joke :point_down: , hope you like it :smile:'
+        }
+        else if (event = 'issues') {
+            greetMsg = 'Thanks for your contribution :blue_heart: .\nContributors :people_holding_hands:  like you make the open source community :earth_africa:  such an amazing place to learn :book: , inspire :angel:, and create :art: .\nWe will investigate :eyes:  and get back to you as soon as possible :+1: . Just make sure you have given us sufficient information :information_source:.\n\nBy that time enjoy this joke :point_down: , hope you like it :smile:'
+        }
 
-        /*
+        console.log(`Running on ${event}......`);
+
+    
         console.log(`eventname: ${github.context.eventName}`)
         console.log(`payload sender: ${JSON.stringify(github.context.payload.sender, undefined, 2)}`)
-        console.log(`workflow: ${github.context.workflow}`)
+        // console.log(`workflow: ${github.context.workflow}`)
         console.log(`payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
 
-*/
-        var issueNumber = context.payload.issue.number;;
-        /*if (context.payload.pull_request.number !== undefined) {
+
+        var issueNumber;
+        if (event === 'pull_request') {
             issueNumber = context.payload.pull_request.number;
-        } else {
+        }
+        else if (event === 'issues') {
             issueNumber = context.payload.issue.number;
         }
-*/
+
         const octokit = github.getOctokit(github_token);
 
-
-
         console.log(`got this joke: ${joke}`)
-
         console.log("commenting...")
-
-
-
+        const owner = context.payload.sender.login;
         const comment = await octokit.issues.createComment({
             issue_number: issueNumber,
             owner: context.payload.repository.owner.login,
             repo: context.payload.repository.name,
-            body: `Hi, ${context.payload.repository.owner.login}\n\nThanks for your contribution. Contributors like you make the open source community such an amazing place to be learn, inspire, and create.\n\nHere is a little gift for you,hope you like it :\n\n${joke}`
+            body: `Hi, @${owner},\n${greetMsg}\n>${joke}\n\nUse this [action](https://github.com/deep5050/MastJokeMara)  on your projects.`
         })
-
-
         core.setOutput('comment-url', comment.data.html_url);
     } catch (error) {
         core.setFailed(error.message);
     }
 }
+
 
 
 
